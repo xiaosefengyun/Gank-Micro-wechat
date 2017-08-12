b60e9173071b0e35cd36117b8e969633e631513b
DESKTOP-7RUBVSE\wf <xiaosefengyun@gmail.com>
2017-08-12 10:18:07 +0800

    update

---
+++ pages/index/constants.js
@@ -0,0 +1,11 @@
+//url相关
+var BASE_URL = "http://gank.io/api";
+var GET_URL = BASE_URL.concat("/history/content/100/1");
+
+var ERROR_DATA_IS_NULL = "获取数据为空，请重试";
+
+module.exports = {
+  BASE_URL: BASE_URL,
+  GET_URL: GET_URL,
+  ERROR_DATA_IS_NULL: ERROR_DATA_IS_NULL
+};
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,83 @@
-//index.js
-//获取应用实例
-var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    items: [],
+    hidden: false
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  onLoad: function (options) {
+    var that = this;
+    requestData(that, mCurrentPage + 1);
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+
+  onItemClick: function (event) {
+    console.log(111);
+  },
+
+  onPostClick: function (event) {
+    console.log('onPostClick');
   }
-})
+
+});
+
+var mTitles = [];
+var mSrcs = [];
+var mTimes = [];
+var mCurrentPage = -1;
+
+function requestData(that, targetPage) {
+  console.log(Constant);
+  wx.request({
+    url: Constant.GET_URL.replace('(/\(\d+))$', targetPage),
+    method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    header: {
+      "Content-Type": "application/json"
+    },
+    success: function (res) {
+      if (res === null ||
+        res.data === null ||
+        res.data.results === null ||
+        res.data.results.length <= 0) {
+        console.error(Constant.ERROR_DATA_IS_NULL);
+        return;
+      }
+
+      for (let i = 0; i < res.data.results.length; i++) {
+        bindData(res.data.results[i]);
+      }
+
+      var itemList = [];
+      for (var i = 0; i < mTitles.length; i++) {
+        itemList.push({
+          title: mTitles[i],
+          src: mSrcs[i],
+          time: mTimes[i]
+        });
+      }
+
+      that.setData({
+        items: itemList,
+        hidden: true
+      })
+
+      mCurrentPage = targetPage;
+
+    },
+    fail: function () {},
+  })
+}
+
+function bindData(itemData) {
+  var re = new RegExp('[a-zA-z]+://[^\"]*');
+  var title = itemData.content.split('img alt=')[1].match(re)[0];
+  if (-1 != (title.search("//ww"))) {
+    var src = title.replace("//ww", "//ws");
+  } else {
+    var src = title;
+  }
+
+  mTitles.push(itemData.title);
+  mTimes.push(itemData.publishedAt.split('T')[0]);
+  mSrcs.push(src);
+}
+
+var Constant = require('./constants');
